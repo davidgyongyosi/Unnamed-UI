cfcc4535b5b418e108856e1a4a814cd66f4cc7c1
Gyöngyösi Dávid <43970367+davidgyongyosi@users.noreply.github.com>
2024-11-06 20:48:10 +0100

    Delete projects/ngx-unnamed-icons/src/assets/outline directory

--- projects/ngx-unnamed-icons/src/assets/outline/check.ts
+++
@@ -1,7 +0,0 @@
-import { IconDefinition } from '../../types';
-
-export const CheckOutline: IconDefinition = {
-  name: 'check',
-  theme: 'outline',
-  icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor"><path d="M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z"/></svg>`
-};
--- projects/ngx-unnamed-icons/src/assets/outline/copy.ts
+++
@@ -1,7 +0,0 @@
-import { IconDefinition } from '../../types';
-
-export const CopyOutline: IconDefinition = {
-  name: 'copy',
-  theme: 'outline',
-  icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor"><path d="M216,32H88a8,8,0,0,0-8,8V80H40a8,8,0,0,0-8,8V216a8,8,0,0,0,8,8H168a8,8,0,0,0,8-8V176h40a8,8,0,0,0,8-8V40A8,8,0,0,0,216,32ZM160,208H48V96H160Zm48-48H176V88a8,8,0,0,0-8-8H96V48H208Z"/></svg>`
-};
