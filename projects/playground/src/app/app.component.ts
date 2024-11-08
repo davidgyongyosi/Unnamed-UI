62bf932ae656f504dc19b8dbabeb6534f5ff6724
Dávid <david.gyongyosi@majin.dev>
2024-11-08 15:48:16 +0100

    icons update

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from '../../../ngx-unnamed/src/lib/components/button/button.component';
-import { AcornFill, AirplaneOutline, IconModule, IconService } from '../../../ngx-unnamed-icons/src/public-api';
-
-
-
+import { AcornFill, AirplaneInFlightFill, IconModule, IconService } from '../../../ngx-unnamed-icons/src/public-api';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AirplaneInFlightOutline, PlusCircleFill } from 'ngx-unnamed-icons';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, IconModule],
+  imports: [RouterOutlet, ButtonComponent, IconModule, HttpClientModule],
   providers: [IconService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -19,7 +18,7 @@ export class AppComponent {
   title = 'playground';
 
   constructor(private _iconService: IconService) {
-    this._iconService.addIcon(...[AcornFill, AirplaneOutline])
+    this._iconService.addIcon(...[AcornFill, AirplaneInFlightFill, AirplaneInFlightOutline, PlusCircleFill]);
   }
 
 }
